96abf42a3db74a25a4e69eeeb23397993906a796
Zachary LaForge <zach.laforge@hotmail.com>
2022-04-13 17:07:14 -0400

    Started Comments views logic + added Comment class to models

--- backend/drf_jwt_backend/youtube_comments/models.py
+++ backend/drf_jwt_backend/youtube_comments/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 class Comment(models.Model):
     user = models.CharField(User, on_delete=models.CASCADE)
-    ideo_id = models.CharField(max_length=100)
+    video_id = models.CharField(max_length=100)
     text = models.CharField(max_length=100)
     likes = models.IntegerField()
     dislikes = models.IntegerField()
--- backend/drf_jwt_backend/youtube_comments/views.py
+++ backend/drf_jwt_backend/youtube_comments/views.py
@@ -1,3 +1,20 @@
+from operator import truediv
 from django.shortcuts import render
+from rest_framework.decorators import APIView
+from backend.drf_jwt_backend.cars import serializers
+from comments.models import Comment
+from .serializers import CommentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+
 
 # Create your views here.
+class CommentList(APIView, AllowAny):
+
+    def get(self, request, video_id):
+        comments = Comment.objects.filter(video_id=video_id)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+
